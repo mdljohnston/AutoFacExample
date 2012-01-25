1e5d871aada2f2ef967632133f936135ecbf1002
mdljohnston@gmail.com <mjohnston@.(none)>
2012-01-25 20:23:24 +1100

    initial commit

---
+++ AutoFacExample/AutoFacExample/Global.asax.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Autofac;
+using Autofac.Integration.Mvc;
+
+namespace AutoFacExample
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            );
+
+        }
+        
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+        }
+    }
+}
